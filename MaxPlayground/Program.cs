fa7a6b7eca2a8eb1a50115a501762877d5bff6c7
Lupova19 <nadiqlupova@gmail.com>
2023-04-25 22:49:44 +0300

    first commit

---
+++ MaxPlayground/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi redove: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Vavedi koloni: ");
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, i] = int.Parse(line[j]);
+                }
+            }
+            int maxValue = int.MinValue;
+            int maxRow = -1;
+            int maxCol = -1;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    if (sum > maxValue)
+                    {
+                        maxValue = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+                Console.WriteLine($"~{matrix[maxRow, maxCol]} {matrix[maxRow, maxCol + 1]}~");
+                Console.WriteLine($"~{matrix[maxRow + 1, maxCol]} {matrix[maxRow + 1, maxCol + 1]}~");
+
+            }
+
+        }
+    }
+}
---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi visocina: ");
+            int h = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[h + 1][];
+
+            for (int row = 0; row < h; row++)
+            {
+                triangle[row] = new long[row + 1];
+
+            }
+            triangle[0][0] = 1;
+            for (int row = 0; row < h - 1; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
+                    Console.Write(triangle[row][col]);
+                }
+                //Console.WriteLine();
+            }
+            for (int row = 0; row < h; row++)
+            {
+                for (int space = 0; space < h - row - 1; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write($"{triangle[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+ }
